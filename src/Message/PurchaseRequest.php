f72ed1f8d96c73a26191cdeae92b4d9a98584856
Gabriel Moreira <gabriel.moreira@photomerchant.net>
2019-01-14 12:18:35 +1100

    Added support for Payeezy refunds.

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -1,79 +1,8 @@
 <?php
-/**
- * Payeezy Purchase Request
- */
 namespace Omnipay\Payeezy\Message;
 
 /**
- * Payeezy Purchase Request
- *
- * ### Example
- *
- * <code>
- * // Create a gateway for the Payeezy Gateway
- * // (routes to GatewayFactory::create)
- * $gateway = Omnipay::create('Payeezy');
- *
- * // Initialise the gateway
- * $gateway->initialize(array(
- *     'gatewayId' => '12341234',
- *     'password'  => 'thisISmyPASSWORD',
- *     'testMode'  => true, // Or false when you are ready for live transactions
- * ));
- *
- * // Create a credit card object
- * $card = new CreditCard(array(
- *     'firstName'            => 'Example',
- *     'lastName'             => 'Customer',
- *     'number'               => '4222222222222222',
- *     'expiryMonth'          => '01',
- *     'expiryYear'           => '2020',
- *     'cvv'                  => '123',
- *     'email'                => 'customer@example.com',
- *     'billingAddress1'      => '1 Scrubby Creek Road',
- *     'billingCountry'       => 'AU',
- *     'billingCity'          => 'Scrubby Creek',
- *     'billingPostcode'      => '4999',
- *     'billingState'         => 'QLD',
- * ));
- *
- * // Do a purchase transaction on the gateway
- * $transaction = $gateway->purchase(array(
- *     'description'              => 'Your order for widgets',
- *     'amount'                   => '10.00',
- *     'transactionId'            => 12345,
- *     'clientIp'                 => $_SERVER['REMOTE_ADDR'],
- *     'card'                     => $card,
- * ));
- *
- * // USE A TRANS-ARMOR TOKEN TO PROCESS A PURCHASE:
- *
- * // Create a credit card object
- * $card = new CreditCard(array(
- *     'firstName'            => 'Example',
- *     'lastName'             => 'Customer',
- *     'expiryMonth'          => '01',
- *     'expiryYear'           => '2020',
- * ));
- *
- * // Do a purchase transaction on the gateway
- * $transaction = $gateway->purchase(array(
- *     'description'              => 'Your order for widgets',
- *     'amount'                   => '10.00',
- *     'cardReference'            => $yourStoredToken,
- *     'clientIp'                 => $_SERVER['REMOTE_ADDR'],
- *     'card'                     => $card,
- *     'tokenCardType'              => 'visa', // MUST BE VALID CONST FROM \omnipay\common\CreditCard
- * ));
- *
- * $response = $transaction->send();
- *
- * if ($response->isSuccessful()) {
- *     echo "Purchase transaction was successful!\n";
- *     $sale_id = $response->getTransactionReference();
- *     echo "Transaction reference = " . $sale_id . "\n";
- * }
- * </code>
+ * Class PurchaseRequest
  */
 class PurchaseRequest extends AbstractRequest
 {
--- src/Message/RefundRequest.php
+++ src/Message/RefundRequest.php
@@ -1,12 +1,8 @@
 <?php
-/**
- * Payeezy Refund Request
- */
-
 namespace Omnipay\Payeezy\Message;
 
 /**
- * Payeezy Refund Request
+ * Class RefundRequest
  */
 class RefundRequest extends AbstractRequest
 {
@@ -14,13 +10,123 @@ class RefundRequest extends AbstractRequest
     {
         $data = parent::getData();
 
-        $this->validate('amount', 'currency');
+        $this->validate('amount', 'currency', 'merchantReference');
 
-        $data['method'] = 'credit_card';
-        $data['amount'] = $this->getAmount();
+        $data['transaction_type'] = 'refund';
+        $data['merchant_ref'] = $this->getMerchantReference();
+        $data['method'] = 'token';
         $data['currency_code'] = $this->getCurrency();
-        $data['transaction_tag'] = $this->getTransactionReference();
+        $data['amount'] = str_replace('.', '', $this->getAmount()); // needs to be in cents
+
+        if ($token = $this->getToken()) {
+            $this->validate('tokenCardType', 'tokenCardHolderName', 'tokenCardExpiry', 'tokenCardCvv');
+
+            $data['token'] = [
+                'token_type' => 'FDToken',
+                'token_data' => [
+                    'type' => $this->getTokenCardType(),
+                    'value' => $token,
+                    'cardholder_name' => $this->getTokenCardHolderName(),
+                    'exp_date' => $this->getTokenCardExpiry(),
+                    'cvv' => $this->getTokenCardCvv(),
+                ],
+            ];
+        }
 
         return $data;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTokenCardType()
+    {
+        return $this->getParameter('tokenCardType');
+    }
+
+    /**
+     * @param string|null $value
+     *
+     * @return \Omnipay\Common\Message\AbstractRequest
+     */
+    public function setTokenCardType($value)
+    {
+        return $this->setParameter('tokenCardType', $value);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTokenCardHolderName()
+    {
+        return $this->getParameter('tokenCardHolderName');
+    }
+
+    /**
+     * @param string|null $value
+     *
+     * @return \Omnipay\Common\Message\AbstractRequest
+     */
+    public function setTokenCardHolderName($value)
+    {
+        return $this->setParameter('tokenCardHolderName', $value);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTokenCardExpiry()
+    {
+        return $this->getParameter('tokenCardExpiry');
+    }
+
+    /**
+     * @param string|null $value
+     *
+     * @return \Omnipay\Common\Message\AbstractRequest
+     */
+    public function setTokenCardExpiry($value)
+    {
+        return $this->setParameter('tokenCardExpiry', $value);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTokenCardCvv()
+    {
+        return $this->getParameter('tokenCardCvv');
+    }
+
+    /**
+     * @param string|null $value
+     *
+     * @return \Omnipay\Common\Message\AbstractRequest
+     */
+    public function setTokenCardCvv($value)
+    {
+        return $this->setParameter('tokenCardCvv', $value);
+    }
+
+    /**
+     * Get Merchant Reference
+     *
+     * @return string
+     */
+    public function getMerchantReference()
+    {
+        return $this->getParameter('merchantReference');
+    }
+
+    /**
+     * Set Merchant Reference
+     *
+     * @param string $value
+     *
+     * @return \Omnipay\Common\Message\AbstractRequest
+     */
+    public function setMerchantReference($value)
+    {
+        return $this->setParameter('merchantReference', $value);
     }
 }
